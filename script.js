641dd3db853fe60e4486a6a0e354bb740fc30a66
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-25 23:24:05 -0500

    Create Book object constructor and addBookToLibrary function. Add basic skeleton to html.

---
+++ script.js
@@ -0,0 +1,15 @@
+const myLibrary = []
+
+function Book(id, title, author, numberOfPages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.read = read;
+}
+
+function addBookToLibrary(title, author, numberOfPages, read) {
+    const id = crypto.randomUUID();
+    const book = Book(id, title, author, numberOfPages, read);
+    myLibrary.push(book);
+}
