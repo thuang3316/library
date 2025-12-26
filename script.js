b029b530e1ad262e0b9d835dd0f5b4b2ad8fcab9
thuang3316 <249622692+thuang3316@users.noreply.github.com>
2025-12-26 14:59:46 -0500

    Complete addBookToPage function

--- script.js
+++ script.js
@@ -10,6 +10,47 @@ function Book(id, title, author, numberOfPages, read) {
 
 function addBookToLibrary(title, author, numberOfPages, read) {
     const id = crypto.randomUUID();
-    const book = Book(id, title, author, numberOfPages, read);
+    const book = new Book(id, title, author, numberOfPages, read);
     myLibrary.push(book);
 }
+
+const tableBody = document.querySelector('tbody');
+
+function addBookToPage(myLibrary) {
+    myLibrary.forEach(book => {
+        let tableRow = document.createElement('tr');
+
+        Object.keys(book).forEach(key => {
+            if (key != 'id') {
+                let tableItem = document.createElement('td');
+                if (key === 'read') {
+                    const readButton = document.createElement('button');
+                    if (book[key] === 'Yes') {
+                        readButton.classList.add('readButton', 'read');
+                        readButton.textContent = 'read';
+                    } else {
+                        readButton.classList.add('readButton', 'unread');
+                        readButton.textContent = 'not read';
+                    }
+                    tableItem.appendChild(readButton);
+                } else {
+                    tableItem.textContent = book[key];
+                }
+                tableRow.appendChild(tableItem);
+            }       
+        })
+        const buttonItem = document.createElement('td');
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'remove';
+        removeButton.classList.add('removeButton');
+        removeButton.dataset.id = book.id;
+
+        buttonItem.appendChild(removeButton);
+        tableRow.appendChild(buttonItem);
+
+        tableBody.appendChild(tableRow);
+    });
+}
+
+addBookToLibrary('1984', 'George Orwell', 328, 'Yes');
+addBookToPage(myLibrary);
